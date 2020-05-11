e8bd71d0320be961db6ce75c01ff8009d3000310
JoshFen95 <jfennell@companieshouse.gov.uk>
2020-05-11 15:16:06 +0100

    exercise attempt

--- src/exercise.py
+++ src/exercise.py
@@ -2,8 +2,8 @@ def main():
     #write your code below this line
     first = input("Greetings! How are you doing?")
     second =input("Oh, how interesting. Tell me more!")
-    thrid = "Thanks for sharing!"
-    
+    thrid = input("Thanks for sharing!")
+
     print (first)
     print (second)
     print (thrid)
